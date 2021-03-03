8c5b6a33dc659954d03cf7267d70f8621e5a4d3c
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2021-03-03 16:42:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace APLICATIVO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("QUAL É O SEU NOME: ");
+            string Nome = Console.ReadLine();
+
+           Console.WriteLine("Seja bem vindo: ", Nome);
+          
+           Console.ReadKey();
+
+        }
+       
+        
+    }
+
+    
+}
+
